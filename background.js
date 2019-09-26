1c5fadc250ec765bd8be740c8db90b9ad6440362
Raykeen <andrayosipov@gmail.com>
2019-09-26 19:44:30 +0500

    prettier

--- background.js
+++ background.js
@@ -1,54 +1,50 @@
 const toggleIn = (tabId, value) => {
-    chrome.storage.local.get("enabledIn", ({ enabledIn }) => {
-        enabledIn = enabledIn || {};
-        enabledIn[tabId] = value;
-        chrome.storage.local.set({enabledIn});
-    });
+  chrome.storage.local.get("enabledIn", ({ enabledIn }) => {
+    enabledIn = enabledIn || {};
+    enabledIn[tabId] = value;
+    chrome.storage.local.set({ enabledIn });
+  });
 };
 
 const isEnabledIn = (tabId, cb) => {
-    chrome.storage.local.get("enabledIn", ({ enabledIn }) => {
-        cb(Boolean(enabledIn && enabledIn[tabId]));
-    });
+  chrome.storage.local.get("enabledIn", ({ enabledIn }) => {
+    cb(Boolean(enabledIn && enabledIn[tabId]));
+  });
 };
 
-const disable = (tabId) => {
-    toggleIn(tabId, false);
+const disable = tabId => {
+  toggleIn(tabId, false);
 
-    chrome.tabs.sendMessage(tabId, {message: "baselinka/disable"});
+  chrome.tabs.sendMessage(tabId, { message: "baselinka/disable" });
 
-    chrome.browserAction.setTitle({ title:"Show baselines", tabId });
-    chrome.browserAction.setBadgeText({ text:"OFF", tabId });
-    chrome.browserAction.setBadgeBackgroundColor({ color:"#121212", tabId });
+  chrome.browserAction.setTitle({ title: "Show baselines", tabId });
+  chrome.browserAction.setBadgeText({ text: "OFF", tabId });
+  chrome.browserAction.setBadgeBackgroundColor({ color: "#121212", tabId });
 };
 
-const enable = (tabId) => {
-    toggleIn(tabId, true);
+const enable = tabId => {
+  toggleIn(tabId, true);
 
-    chrome.tabs.sendMessage(tabId, {message: "baselinka/enable"});
-    chrome.browserAction.setTitle({ title: "Hide baselines", tabId });
-    chrome.browserAction.setBadgeText({ text:"ON", tabId });
-    chrome.browserAction.setBadgeBackgroundColor({ color:"#A6E22E", tabId });
+  chrome.tabs.sendMessage(tabId, { message: "baselinka/enable" });
+  chrome.browserAction.setTitle({ title: "Hide baselines", tabId });
+  chrome.browserAction.setBadgeText({ text: "ON", tabId });
+  chrome.browserAction.setBadgeBackgroundColor({ color: "#A6E22E", tabId });
 };
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-    isEnabledIn(tab.id, (isEnabled) => {
-        if (isEnabled)
-            disable(tab.id);
-        else
-            enable(tab.id);
-    })
+  isEnabledIn(tab.id, isEnabled => {
+    if (isEnabled) disable(tab.id);
+    else enable(tab.id);
+  });
 });
 
-chrome.tabs.onCreated.addListener((tab) => {
-    disable(tab.id);
+chrome.tabs.onCreated.addListener(tab => {
+  disable(tab.id);
 });
 
-chrome.tabs.onUpdated.addListener((tabId) => {
-    isEnabledIn(tabId, (isEnabled) => {
-        if (isEnabled)
-            enable(tabId);
-        else
-            disable(tabId);
-    })
+chrome.tabs.onUpdated.addListener(tabId => {
+  isEnabledIn(tabId, isEnabled => {
+    if (isEnabled) enable(tabId);
+    else disable(tabId);
+  });
 });
--- content.js
+++ content.js
@@ -1,88 +1,86 @@
 const baselinka = document.createElement("div");
 
-const getRelativeBaselinePosition = (el) => {
-    const clone = el.cloneNode(true);
-    const computedStyle = getComputedStyle(el);
-    clone.style.cssText = computedStyle.cssText;
-    clone.style.height = `${el.offsetHeight}px`;
-    clone.style.width = `${el.offsetWidth}px`;
-    clone.style.boxSizing = "border-box";
+const getRelativeBaselinePosition = el => {
+  const clone = el.cloneNode(true);
+  const computedStyle = getComputedStyle(el);
+  clone.style.cssText = computedStyle.cssText;
+  clone.style.height = `${el.offsetHeight}px`;
+  clone.style.width = `${el.offsetWidth}px`;
+  clone.style.boxSizing = "border-box";
 
-    const container = document.createElement("div");
-    const marker = document.createElement("div");
+  const container = document.createElement("div");
+  const marker = document.createElement("div");
 
-    if ("inline" === computedStyle.display) {
-        marker.style.cssText = `
+  if ("inline" === computedStyle.display) {
+    marker.style.cssText = `
             display: inline-block;
             vertical-align: baseline;
         `;
-    } else {
-        container.style.cssText = `
+  } else {
+    container.style.cssText = `
             display: flex;
             align-items: baseline;
         `;
-        marker.style.cssText = `
+    marker.style.cssText = `
             display: inline-flex;
         `;
-    }
+  }
 
-    container.appendChild(marker);
-    container.appendChild(clone);
+  container.appendChild(marker);
+  container.appendChild(clone);
 
-    document.body.appendChild(container);
+  document.body.appendChild(container);
 
-    const diff = marker.offsetTop - clone.offsetTop;
+  const diff = marker.offsetTop - clone.offsetTop;
 
-    document.body.removeChild(container);
+  document.body.removeChild(container);
 
-    return diff;
+  return diff;
 };
 
-const handleElementEvent = ({target}) => {
-    if (target === baselinka) {
-        return;
-    }
+const handleElementEvent = ({ target }) => {
+  if (target === baselinka) {
+    return;
+  }
 
-    const {top, left, width} = target.getBoundingClientRect();
-    const bl = getRelativeBaselinePosition(target);
+  const { top, left, width } = target.getBoundingClientRect();
+  const bl = getRelativeBaselinePosition(target);
 
-    baselinka.style.cssText = `
-		position: absolute;
-		transition: all 0.3s cubic-bezier(0,1,.5,1);
-		background: red;
-		height: 1px;
-		top: 0px;
-		transform: translateY(${top + bl + window.pageYOffset}px);
-		left: ${left}px;
-		width: ${width}px;
-	`;
+  baselinka.style.cssText = `
+        position: absolute;
+        transition: all 0.3s cubic-bezier(0,1,.5,1);
+        background: red;
+        height: 1px;
+        top: 0px;
+        transform: translateY(${top + bl + window.pageYOffset}px);
+        left: ${left}px;
+        width: ${width}px;
+    `;
 };
 
 const enable = () => {
-    if (baselinka.parentNode !== document.body) {
-        document.body.appendChild(baselinka);
-    }
+  if (baselinka.parentNode !== document.body) {
+    document.body.appendChild(baselinka);
+  }
 
-    document.addEventListener("mouseover", handleElementEvent);
+  document.addEventListener("mouseover", handleElementEvent);
 };
 
 const disable = () => {
-    if (baselinka.parentNode === document.body) {
-        document.body.removeChild(baselinka);
-    }
+  if (baselinka.parentNode === document.body) {
+    document.body.removeChild(baselinka);
+  }
 
-    document.removeEventListener("mouseover", handleElementEvent);
+  document.removeEventListener("mouseover", handleElementEvent);
 };
 
-chrome.runtime.onMessage.addListener(
-    ({message}) => {
-        switch (message) {
-            case "baselinka/enable":
-                enable();
-                break;
-            case "baselinka/disable":
-                disable();
-                break;
-        }
-    }
-);
+chrome.runtime.onMessage.addListener(({ message }) => {
+  switch (message) {
+    case "baselinka/enable":
+      enable();
+      break;
+    case "baselinka/disable":
+      disable();
+      break;
+  }
+});
